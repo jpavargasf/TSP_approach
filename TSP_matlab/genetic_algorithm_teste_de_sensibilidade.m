a531e4ab50c154155f2352e4afd1674bf7a5d44f
rks6 <39680560+jpavargasf@users.noreply.github.com>
2022-04-04 23:31:56 -0300

    paper

--- TSP_matlab/genetic_algorithm_teste_de_sensibilidade.m
+++ TSP_matlab/genetic_algorithm_teste_de_sensibilidade.m
@@ -3,14 +3,14 @@
 %Trabalho desenvolvido para a disciplina de Sistemas Inteligentes do Curso
 %de Engenharia Eletrônica da Universidade Tecnológica Federal do Paraná
 %
-%Comentários:
+%Comentários:demora uma hora pra rodar este código 
 
 tic;
 
 
 rng('shuffle');
 
-n_population = 200;
+n_population = 1000;
 %n_parents = 2;
 
 
@@ -31,10 +31,18 @@ for n_cities = 3:1:100
     [path,cost_history] = genetic_algorithm(cities,n_population,max_iterations,p_mutation,p_genes_equal,pdf);
     n_iter(n_cities-2) = length(cost_history);
     x(n_cities-2) = n_cities;
+    sens(n_cities-2) = 100*(cost_history(1)/cost_history(end)-1);
 end
 
 figure, plot(x,n_iter,'DisplayName',"Número de Gerações"),xlabel("Número de cidades"),ylabel("Número de gerações");
 m = repmat(mean(n_iter),1,length(n_iter));
+hold on;
+plot(x,m,'DisplayName',"Média");
+legend;
+
+figure, plot(x,sens,'DisplayName',"Melhoria"),xlabel("Número de cidades"),ylabel("Melhoria da população final");
+ytickformat('percentage');
+m = repmat(mean(sens),1,length(sens));
 hold on;
 plot(x,m,'DisplayName',"Média");
 legend;
